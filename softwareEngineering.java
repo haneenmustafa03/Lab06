cbb3601d20867428fb38df52888ca912da3cdeae
haneenmustafa03 <haneenmustafa500@gmail.com>
2022-03-19 21:28:46 -0400

    COMMITTING

--- softwareEngineering.java
+++ softwareEngineering.java
@@ -9,7 +9,7 @@ public class softwareEngineering {
         System.out.println("Input second number");
         int num2 = scanner.nextInt();
 
-        int num3 = 3;
+        int num3 = 4;
         System.out.println(num1 + num2);
         
     }
